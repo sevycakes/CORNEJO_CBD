aa42b1ab492e83ed536af13c476994356c6beeb5
unknown <christian_yves_cornejo@dlsl.edu.ph>
2025-03-05 22:27:21 +0800

    i tried

---
+++ script.js
@@ -0,0 +1,13 @@
+// const slider = document.querySelector(".banner__slider");
+// const images = slider.children;
+// let index = 0;
+
+// function slide() {
+//     index++;
+//     if (index >= images.length) {
+//         index = 0; // Reset to first image after the last one
+//     }
+//     slider.style.transform = `translateY(-${index * 250}px)`; // Move by the height of one image
+// }
+
+// setInterval(slide, 3000); // Auto-scroll every 3 seconds
